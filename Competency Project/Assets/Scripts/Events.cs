0a813cd769bdc6b80a5792f893cfacbdf21e6985
Christian Hansen <chr.hansen@live.dk>
2016-10-13 10:55:14 +0200

    Game

---
+++ Competency Project/Assets/Scripts/Events.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class TakeDamageEvent : GameEvent
+{
+    public string message { get; private set; }
+
+    public TakeDamageEvent(string message)
+    {
+        this.message = message;
+    }
+}
+
+}
+
