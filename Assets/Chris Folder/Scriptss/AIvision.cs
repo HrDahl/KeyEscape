90c97d6b9b861086083908df522a99bdcbe67f0f
Christian <chan12@student.aau.dk>
2016-10-16 15:25:01 +0200

    Vision, patrolling and standing added

---
+++ Assets/Chris Folder/Scriptss/AIvision.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIvision : MonoBehaviour
+{
+
+	public float fieldOfView = 30.0f;
+	public Vector3 lastSighting; 
+	public float range = 10f;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle (direction, transform.forward);
+
+			if (angle < fieldOfView * 0.5) {
+				//Debug.Log ("Sighted!");
+				RaycastHit hit;
+
+				if (Physics.Raycast (transform.position , direction.normalized, out hit, range)) {
+					Debug.Log (hit);
+					if (hit.collider.gameObject.tag == "Player") {
+						Debug.Log ("Sighted!");
+						lastSighting = hit.transform.position;
+						EventManager.Instance.TriggerEvent (new PlayerSpottedEvent (lastSighting));
+					}
+
+				}
+			
+			}
+
+		}
+	}
+}
