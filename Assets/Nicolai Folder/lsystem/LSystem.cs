1167b5859ede8579d2969446914d8d8383ee2da1
Nicolai <nico_goal@hotmail.com>
2016-10-17 15:53:42 +0200

    Procedural Leaf on time

--- Assets/Nicolai Folder/lsystem/LSystem.cs
+++ Assets/Nicolai Folder/lsystem/LSystem.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class LSystem : MonoBehaviour {
 	List<LSElement> str;
-	public int length0 = 100/1;
+	int length0 = 1;
 	public float w0 = 20;
 	public float alpha1 = 30;
 	public float alpha2 = -30;
@@ -16,31 +16,51 @@ public class LSystem : MonoBehaviour {
 	public float e = 0.5f;
 	public float smin = 0;
 	public int iter = 8;
+    public GameObject mappleLeaf;
 
 	List<Vector3> vertices = new List<Vector3>();
 	List<int> indices = new List<int>();
 	int count = 0;
 
+    GameObject treeSide;
+    Mesh mesh;
+
+    List<Vector3> normals = new List<Vector3>();
+
 	void Start(){
-		str = new List<LSElement>();
+        treeSide = (GameObject)GameObject.FindGameObjectWithTag("Tree");
 
+        if (treeSide == null) {
+            str = new List<LSElement>();
+            ExpandRules();
+
+            mesh = Interpret();
+            mesh.RecalculateNormals();
+            mesh.uv = new Vector2[mesh.vertexCount];
+            mesh.RecalculateBounds();
+            mesh.Optimize();
+
+            GetComponent<MeshFilter>().mesh = mesh;
+        } else {
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
 	}
 
-	void Update(){
-		if (count % 10 == 0) {
-			ExpandRules();
+    void Update() {
+       /* if (count % 10 == 0) {
+            ExpandRules();
 
-			var mesh = Interpret();
-			mesh.RecalculateNormals();
-			mesh.uv = new Vector2[mesh.vertexCount];
+            mesh = Interpret();
+            mesh.RecalculateNormals();
+            mesh.uv = new Vector2[mesh.vertexCount];
 
-			GetComponent<MeshFilter>().mesh = mesh;
-		}
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
 
-		count++;
-	}
+        count++; */
+    }
 
-	void ExpandRules(){
+	void ExpandRules() {
 		str.Clear();
 		str.Add(new LSElement(LSElement.LSSymbol.A, length0, w0));
 
@@ -58,7 +78,11 @@ public class LSystem : MonoBehaviour {
 	}
 
 	void AddCone(Matrix4x4 m, float l, float w0, float w1){
-		const int N = 5;
+        int N = 5;
+        if (w0 < 0.01f) {
+            N = 3;
+        }
+
 		for (int i = 0; i <= N; i++){
 			
 			float alpha = 2.0f*Mathf.PI*i/(float)N;
@@ -77,37 +101,61 @@ public class LSystem : MonoBehaviour {
 			vertices.Add(p2);
 			vertices.Add(p3);
 
-			for (int j = 0;j < 6; j++){
+            if (w0 < 0.01f) {
+                Vector3 side1 = p2 - p0;
+                Vector3 side2 = p1 - p0;
+                Vector3 perp = Vector3.Cross(side1, side2);
+                var perpLength = perp.magnitude;
+                perp /= perpLength;
+
+                Vector3 randomPoint1 = (1 - Mathf.Sqrt(Random.Range(0.0f, 1.0f))) * p0 + (Mathf.Sqrt(Random.Range(0.0f, 1.0f)) * (1 - Random.Range(0.0f, 1.0f))) * p1;
+
+                Debug.Log("Point: " + p3);
+                Vector3 normal = p1 + (p2 - p1) + Random.Range(0.0f, 1.0f) * (p3 - p1);
+                Debug.Log(perp);
+                normals.Add(perp);
+            }
+
+			for (int j = 0; j < 6; j++){
 				indices.Add(indices.Count);
 			}
+
+
 		}
+        //Branch done
+        Debug.Log("New Branch:");
 	}
 
 	public Mesh Interpret(){
 		Turtle turtle = new Turtle(w0);
-
+        Debug.Log(str.Count);
 		foreach (var elem in str){
 			switch (elem.symbol){
 			case LSElement.LSSymbol.DRAW:
-				AddCone(turtle.Peek().M, elem.data[0], turtle.GetWidth(), turtle.GetWidth() * elem.data[1]);
+                    //Debug.Log("DRAW : " + turtle.Peek().M);
+                    AddCone(turtle.Peek().M, elem.data[0], turtle.GetWidth(), turtle.GetWidth() * elem.data[1]);
 				turtle.Move(elem.data[0]);
 				break;
 			case LSElement.LSSymbol.TURN:
-				turtle.Turn(elem.data[0]);
+                    //Debug.Log("TURN : " + turtle.Peek().M);
+                    turtle.Turn(elem.data[0]);
 				break;
 			case LSElement.LSSymbol.ROLL:
 				turtle.Roll(elem.data[0]);
 				break;
-			case LSElement.LSSymbol.LEFT_BRACKET:
+                case LSElement.LSSymbol.LEFT_BRACKET:
+                    //Debug.Log("PUSH (NEW BRANCH) : " + turtle.Peek().M);
 				turtle.Push();
 				break;
 			case LSElement.LSSymbol.RIGHT_BRACKET:
-				turtle.Pop();
+                    //Debug.Log("POP (GO BACK BRANCH) : " + turtle.Peek().M);
+                    turtle.Pop();
 				break;
 			case LSElement.LSSymbol.WIDTH:
-				turtle.SetWidth(elem.data[0]);
+                    //Debug.Log("WIDTH : " + turtle.Peek().M);
+                    turtle.SetWidth(elem.data[0]);
 				break;
-			}
+            }
 		}
 
 		Mesh mesh = new Mesh();
@@ -116,126 +164,5 @@ public class LSystem : MonoBehaviour {
 		vertices.Clear();
 		indices.Clear();
 		return mesh;
-	}
-
-	void OnGUI(){
-		if (GUI.Button(new Rect(0,0,50,30), "Fig.a")){
-			r1 = 0.75f;
-			r2 = 0.77f;
-			alpha1 = 35;
-			alpha2 = -35;
-			phi1 = 0;
-			phi2 = 0;
-			w0 = 30;
-			q = 0.5f;
-			e = 0.4f;
-			smin = 0;
-			iter = 10;
-		}
-		if (GUI.Button(new Rect(50,0,50,30), "Fig.b")){
-			r1 = 0.65f;
-			r2 = 0.71f;
-			alpha1 = 27;
-			alpha2 = -68;
-			phi1 = 0;
-			phi2 = 0;
-			w0 = 20;
-			q = 0.53f;
-			e = 0.5f;
-			smin = 1.7f;
-			iter = 10;
-		}
-		if (GUI.Button(new Rect(100,0,50,30), "Fig.c")){
-			r1 = 0.5f;
-			r2 = 0.85f;
-			alpha1 = 25;
-			alpha2 = -15;
-			phi1 = 180;
-			phi2 = 0;
-			w0 = 20;
-			q = 0.45f;
-			e = 0.5f;
-			smin = 0.5f;
-			iter = 9;
-		}
-		if (GUI.Button(new Rect(150,0,50,30), "Fig.d")){
-			r1 = 0.6f;
-			r2 = 0.85f;
-			alpha1 = 25;
-			alpha2 = -15;
-			phi1 = 180;
-			phi2 = 180;
-			w0 = 20;
-			q = 0.45f;
-			e = 0.5f;
-			smin = 0.0f;
-			iter = 10;
-		}
-		if (GUI.Button(new Rect(200,0,50,30), "Fig.e")){
-			r1 = 0.58f;
-			r2 = 0.83f;
-			alpha1 = 30;
-			alpha2 = 15;
-			phi1 = 0;
-			phi2 = 180;
-			w0 = 20;
-			q = 0.40f;
-			e = 0.5f;
-			smin = 1.0f;
-			iter = 10;
-		}
-		if (GUI.Button(new Rect(250,0,50,30), "Fig.f")){
-			r1 = 0.92f;
-			r2 = 0.37f;
-			alpha1 = 0;
-			alpha2 = 60;
-			phi1 = 180;
-			phi2 = 0;
-			w0 = 2;
-			q = 0.50f;
-			e = 0.0f;
-			smin = 0.5f;
-			iter = 12;
-		}
-		if (GUI.Button(new Rect(300,0,50,30), "Fig.g")){
-			r1 = 0.80f;
-			r2 = 0.80f;
-			alpha1 = 30;
-			alpha2 = -30;
-			phi1 = 137;
-			phi2 = 137;
-			w0 = 30;
-			q = 0.50f;
-			e = 0.5f;
-			smin = 0.0f;
-			iter = 10;
-		}
-		if (GUI.Button(new Rect(350,0,50,30), "Fig.h")){
-			r1 = 0.95f;
-			r2 = 0.75f;
-			alpha1 = 5;
-			alpha2 = -30;
-			phi1 = -90;
-			phi2 = 90;
-			w0 = 40;
-			q = 0.60f;
-			e = 0.45f;
-			smin = 25.0f;
-			iter = 12;
-		}
-		if (GUI.Button(new Rect(400,0,50,30), "Fig.i")){
-			r1 = 0.55f;
-			r2 = 0.95f;
-			alpha1 = -5;
-			alpha2 = 30;
-			phi1 = 137;
-			phi2 = 137;
-			w0 = 5;
-			q = 0.40f;
-			e = 0.00f;
-			smin = 5.0f;
-			iter = 12;
-		}
-
 	}
 }
