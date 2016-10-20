d6b1d2b4e9e573f49c7217d5949250d4c0842e40
Christian Hansen <chr.hansen@live.dk>
2016-10-20 15:14:44 +0200

    player controller

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxHealth = 3f;
+	public float currentHealth; 
+
+	void OnEnable() {
+		currentHealth = maxHealth;
+		EventManager.Instance.StartListening<TakeDamageEvent>(TakeDamage);
+	}
+
+	void OnDestroy() {
+		EventManager.Instance.StopListening <TakeDamageEvent>(TakeDamage);
+	}
+		
+	private void TakeDamage(TakeDamageEvent e) {
+
+		currentHealth -= e.damage;
+
+		if (currentHealth <= 0) {
+			Debug.Log ("Ded");
+		}
+
+	}
+}
