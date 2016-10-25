f889f01aa93a618278cb397903d4d979f14d7dcc
John <irelandjj@gmail.com>
2016-10-25 11:59:43 +0200

    APPEARANCE

--- Assets/KeyManager.cs
+++ Assets/KeyManager.cs
@@ -6,6 +6,7 @@ public class KeyManager : MonoBehaviour {
 
     public Image[] keyList; 
     public Sprite[] keyMaterials;
+	private GameObject player;
 
     void OnEnable() {
         EventManager.Instance.StartListening<PickUpKey>(KeyPickUp);
@@ -16,19 +17,23 @@ public class KeyManager : MonoBehaviour {
     }
 
     private void KeyPickUp(PickUpKey e) {
-        Debug.Log(e.key.name);
+		player = GameObject.FindGameObjectWithTag("Ethan");
         switch (e.key.name) {
             case "Green Key":
                 keyList[0].GetComponent<Image>().sprite = keyMaterials[0];
+				player.GetComponent<Renderer> ().material.color = Color.green;
                 break;
             case "Blue Key":
                 keyList[1].GetComponent<Image>().sprite = keyMaterials[1];
+				player.GetComponent<Renderer> ().material.color = Color.blue;
                 break;
             case "Red Key":
                 keyList[2].GetComponent<Image>().sprite = keyMaterials[2];
+				player.GetComponent<Renderer> ().material.color = Color.red;
                 break;
             case "Rainbow Key":
                 keyList[3].GetComponent<Image>().sprite = keyMaterials[3];
+				player.GetComponent<Renderer> ().material.color = Color.white;
                 break;
             default:
                 break;
