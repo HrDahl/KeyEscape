a51cfbef871ca30925ac1a575d68618f8f99cc26
Nicolai <nico_goal@hotmail.com>
2016-10-25 14:32:55 +0200

    fefe

--- Assets/roomTimer.cs
+++ Assets/roomTimer.cs
@@ -22,8 +22,14 @@ public class roomTimer : MonoBehaviour {
 
     public void StartTimer(StartTimer e) {
 
-        timeAmount = e.Time;
-        beginTime = true;
+        if (e.Time > 0f) {
+            timeAmount = e.Time;
+        }
+        beginTime = beginTime ? false : true;
+
+        if (!beginTime) {
+            StartCoroutine(doneLevel(7f));
+        }
 
     }
 
@@ -65,5 +71,69 @@ public class roomTimer : MonoBehaviour {
                 EventManager.Instance.TriggerEvent(new DeadEvent());
             }
         }
+    }
+
+    IEnumerator doneLevel(float secondsToWait) {
+
+        GetComponent<RectTransform>().localPosition = new Vector3(0, -55f, 0);
+        GetComponent<RectTransform>().sizeDelta = new Vector2(0, 250);
+        string timeStr = timeAmount.ToString("n2");
+        string seconds = timeStr.Substring(0, 2);
+        string miliseconds = timeStr.Substring(2);
+
+        string color1 = "";
+        string color2 = "";
+
+        if (timeAmount < 10f) {
+            seconds = timeStr.Substring(0, 1);
+            miliseconds = timeStr.Substring(1);
+        }
+
+        string completed = "";
+        int i = 0;
+        foreach (char c in "Completed!") {
+
+            switch (i) {
+                case 0:
+                    completed += "<color=#FF0000><size=100>" + c + "</size></color>";
+                    break;
+                case 1:
+                    completed += "<color=#ff7200><size=100>" + c + "</size></color>";
+                    break;
+                case 2:
+                    completed += "<color=#ffcc00><size=100>" + c + "</size></color>";
+                    break;
+                case 3:
+                    completed += "<color=#eeff00><size=100>" + c + "</size></color>";
+                    break;
+                case 4:
+                    completed += "<color=#aeff00><size=100>" + c + "</size></color>";
+                    break;
+                case 5:
+                    completed += "<color=#6aff00><size=100>" + c + "</size></color>";
+                    break;
+                case 6:
+                    completed += "<color=#00ff3f><size=100>" + c + "</size></color>";
+                    break;
+                case 7:
+                    completed += "<color=#00ffd0><size=100>" + c + "</size></color>";
+                    break;
+                case 8:
+                    completed += "<color=#00ffff><size=100>" + c + "</size></color>";
+                    break;
+                case 9:
+                    completed += "<color=#00b2ff><size=100>" + c + "</size></color>";
+                    break;
+                default:
+                    break;
+            }
+
+            i++;
+        }
+
+        GetComponent<Text>().lineSpacing = 1.5f;
+        GetComponent<Text>().text = completed + "\n" + "<color=white><size=80>" + seconds  + "</size></color>" + "<color=white><size=40>" + miliseconds + " seconds</size></color>";
+        yield return new WaitForSeconds(secondsToWait);
+        GetComponent<Text>().text = "";
     }
 }
