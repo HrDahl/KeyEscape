4e982e20045b946016dad0225a739c15b19a73d7
Christian <chan12@student.aau.dk>
2016-10-26 14:24:05 +0200

    sound fx

--- Assets/_Scripts/KeyScript.cs
+++ Assets/_Scripts/KeyScript.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class KeyScript : MonoBehaviour {
 
+	public GameObject audioGO;
+	AudioSource audio;
+
+	void Start() {
+		audio = audioGO.GetComponent<AudioSource> ();
+
+	}
+
 	void Update () {
         transform.Rotate(new Vector3(0f, 30f, 0f) * Time.deltaTime * 3);
 	}
 
     void OnTriggerEnter(Collider other){
         if (other.gameObject.CompareTag ("Player")) {
-            
+			audio.Play ();
             EventManager.Instance.TriggerEvent(new PickUpKey(gameObject));
 
             gameObject.SetActive(false);
